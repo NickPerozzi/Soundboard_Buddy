5a2dd543a97e0684b7df45e350b72f6e8ead6180
NickPerozzi <53713045+NickPerozzi@users.noreply.github.com>
2021-11-03 16:38:32 -0500

    Recording buttons work! Not saving them yet

--- app/src/main/java/com/perozzi_package/soundboardbuddy/ui/soundboard/AddItemDialogFragment.kt
+++ app/src/main/java/com/perozzi_package/soundboardbuddy/ui/soundboard/AddItemDialogFragment.kt
@@ -1,17 +1,26 @@
 package com.perozzi_package.soundboardbuddy.ui.soundboard
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
+import android.media.MediaPlayer
+import android.media.MediaRecorder
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDialogFragment
+import androidx.core.app.ActivityCompat
 import androidx.navigation.NavController
 import com.perozzi_package.soundboardbuddy.data.db.entities.SoundGridItem
 import kotlinx.android.synthetic.main.fragment_add_item_dialog.*
 import soundboardbuddy.R
 import soundboardbuddy.databinding.FragmentAddItemDialogBinding
+import java.io.File
 
 class AddItemDialogFragment(context: Context, private var addDialogListener: AddDialogListener) :
     AppCompatDialogFragment() {
@@ -19,6 +28,7 @@ class AddItemDialogFragment(context: Context, private var addDialogListener: Add
     private lateinit var navController: NavController
     private lateinit var binding: FragmentAddItemDialogBinding
     private lateinit var addItemDialogViewModel: AddItemDialogViewModel
+    private lateinit var mediaRecorder: MediaRecorder
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,6 +46,25 @@ class AddItemDialogFragment(context: Context, private var addDialogListener: Add
         addItemDialogViewModel = AddItemDialogViewModel()
         binding.addItemDialogViewModel = addItemDialogViewModel
 
+        startRecordAudioButton.isEnabled = true /*false*/
+        stopRecordAudioButton.isEnabled = false
+        previewRecordAudioButton.isEnabled = false
+
+        if (ActivityCompat.checkSelfPermission(
+                (requireActivity() as AppCompatActivity),
+                Manifest.permission.RECORD_AUDIO
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                (requireActivity() as AppCompatActivity),
+                arrayOf(
+                    Manifest.permission.RECORD_AUDIO,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ), 111
+            )
+            startRecordAudioButton.isEnabled = true
+        }
+
         // val uploadRadioButton = binding.uploadRadioButton
         // val recordRadioButton = binding.recordRadioButton
         uploadRadioButton.isSelected = true
@@ -50,15 +79,15 @@ class AddItemDialogFragment(context: Context, private var addDialogListener: Add
                     addItemDialogViewModel.recordSelectedNotUpload.postValue(false)
                     Toast.makeText(context, "Upload time", Toast.LENGTH_SHORT).show()
                     uploadAudioButton.visibility = View.VISIBLE
-                    recordAudioButton.visibility = View.GONE
-                    rerecordAudioButton.visibility = View.GONE
+                    startRecordAudioButton.visibility = View.GONE
+                    stopRecordAudioButton.visibility = View.GONE
                 }
                 R.id.recordRadioButton -> {
                     addItemDialogViewModel.recordSelectedNotUpload.value = true
                     Toast.makeText(context, "Recording time", Toast.LENGTH_SHORT).show()
                     uploadAudioButton.visibility = View.GONE
-                    recordAudioButton.visibility = View.VISIBLE
-                    rerecordAudioButton.visibility = View.VISIBLE
+                    startRecordAudioButton.visibility = View.VISIBLE
+                    stopRecordAudioButton.visibility = View.VISIBLE
                 }
                 else -> {
                 }
@@ -70,6 +99,57 @@ class AddItemDialogFragment(context: Context, private var addDialogListener: Add
             startFileChooser()
 */
         }
+
+        // val recordingPath = context?.getExternalFilesDir(null)?.absolutePath + "latest_recording.mp3"
+        val recordingPath = requireContext().getExternalFilesDir("")?.absolutePath + "latest_recording.mp3"
+        val mediaPlayer = MediaPlayer()
+        mediaRecorder = MediaRecorder()
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC)
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+        mediaRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB)
+        mediaRecorder.setOutputFile(recordingPath)
+
+        // val recording = mediaPlayer.setDataSource(recordingPath)
+
+        // Starts the recording
+        startRecordAudioButton.setOnClickListener {
+            mediaRecorder.prepare()
+            mediaRecorder.start()
+            startRecordAudioButton.isEnabled = false
+            stopRecordAudioButton.isEnabled = true
+            previewRecordAudioButton.isEnabled = false
+            resetRecordAudioButton.isEnabled = false
+        }
+
+        // Stops the recording
+        stopRecordAudioButton.setOnClickListener {
+            mediaRecorder.stop()
+            startRecordAudioButton.isEnabled = false
+            stopRecordAudioButton.isEnabled = false
+            previewRecordAudioButton.isEnabled = true
+            resetRecordAudioButton.isEnabled = true
+
+        }
+
+        // Plays back the recording
+        previewRecordAudioButton.setOnClickListener {
+            mediaPlayer.setDataSource(recordingPath)
+            mediaPlayer.prepare()
+            mediaPlayer.start()
+        }
+
+        resetRecordAudioButton.setOnClickListener {
+            mediaPlayer.reset()
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC)
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+            mediaRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB)
+            mediaRecorder.setOutputFile(recordingPath)
+            startRecordAudioButton.isEnabled = true
+            stopRecordAudioButton.isEnabled = false
+            previewRecordAudioButton.isEnabled = false
+            resetRecordAudioButton.isEnabled = false
+        }
+
 
         confirmButton.setOnClickListener {
             val name = editTextSoundName.text.toString()
@@ -87,6 +167,17 @@ class AddItemDialogFragment(context: Context, private var addDialogListener: Add
             dismiss()
         }
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode==111 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            startRecordAudioButton.isEnabled = true
+    }
+
 }
 
 
